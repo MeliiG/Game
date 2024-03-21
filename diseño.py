6e7050b8d010a29ff9d911bb2947275f026f6664
MeliiG <meli.bogar15@gmail.com>
2024-03-21 15:47:00 -0500

    ladders&snakes

--- diseño.py
+++ diseño.py
@@ -1,10 +1,11 @@
 import pygame
 
 # Definir colores
-BLANCO = (255, 255, 255)
-GRIS_CLARO = (200, 200, 200)
-FUCSIA = (255, 0, 255)
+BLANCO = (255, 255, 255,128)
+GRIS_CLARO = (200, 200, 200,128)
+FUCSIA = (128, 0, 128)
 AZUL = (64, 224, 208)
+AZULL=(65, 105, 225)
 NEGRO = (0, 0, 0)
 AMARILLO = (255, 255, 0)
 ROJO = (255, 0, 0)
@@ -26,8 +27,10 @@ POSICION_INICIAL = (9, 0)
 
 # Escaleras y Serpientes
 
-serpientes = {16:6 , 43:35 , 89:4  , 93:61 , 95:39 }
-escaleras = {2:13, 19:82 , 22:53 , 66:87 , 49:98 }  
+serpientes = {24: 8, 25: 12, 39: 22, 50: 28 , 63:45}  # Diccionario de serpientes con el tablero hecho
+escaleras = {5: 13, 23: 40, 26: 41, 29: 46 , 52:59}  # Diccionario de escaleras con el tablero hecho
+
+ 
 #escaleras = {2: 38, 4: 14, 9: 31, 21: 42, 28: 84, 36: 44, 51: 67, 71: 91, 80: 100}
 #serpientes = {16: 6, 47: 26, 49: 11, 56: 53, 62: 19, 64: 60, 87: 24, 93: 73, 95: 75, 98: 78}
 
